c9aae73e1d36511f07dcf2dce7401f30dbb3f35f
Jochem Smit <jhsmit@gmail.com>
2025-12-01 16:26:05 +0100

    make sure _root_data is not used as a key

--- hal/config.py
+++ hal/config.py
@@ -2,7 +2,7 @@
 
 from dataclasses import asdict, dataclass, field
 from pathlib import Path
-from typing import Optional
+from typing import Optional, TypeVar, Generic
 
 import yaml
 from dacite import Config as DaciteConfig
@@ -10,8 +10,37 @@ from dacite import from_dict
 from dacite.data import Data
 
 from hal.utils import clean_types
+import os
+import sys
 
-ROOT = Path(__file__).parent.parent.absolute()
+
+def has_rootfiles(pth: Path) -> bool:
+    root_files = ["pyproject.toml", "config.yaml"]
+    return all((pth / f).exists() for f in root_files)
+
+
+def find_project_root() -> Path:
+    """Find the project root by looking for specific files in parent directories."""
+    if env_root := os.getenv("HAL_PROJECT_ROOT"):
+        return Path(env_root).absolute()
+
+    executable_path = Path(sys.executable)
+    folder = executable_path.parts[-3]  # go up two levels
+    maybe_root = Path(*executable_path.parts[:-3])
+    if folder == ".venv" and has_rootfiles(maybe_root):
+        return maybe_root.absolute()
+
+    current_path = Path.cwd().absolute()
+    for parent in [current_path] + list(current_path.parents):
+        if has_rootfiles(parent):
+            return parent
+
+    raise FileNotFoundError(
+        "Project root with required files not found. Configure manually by setting HAL_PROJECT_ROOT environment variable."
+    )
+
+
+ROOT = find_project_root()
 
 
 @dataclass
@@ -22,16 +51,56 @@ class Cluster:
     memory_limit: Optional[str] = None
 
 
+K = TypeVar("K")
+V = TypeVar("V")
+
+
+class MemoryDict(dict, Generic[K, V]):
+    """dict which keeps track of accessed keys"""
+
+    RESERVED_KEYS = frozenset(["_root_data"])
+
+    def __init__(self, *args, **kwargs):
+        initial_data = dict(*args, **kwargs)
+        if reserved := self.RESERVED_KEYS & initial_data.keys():
+            raise KeyError(f"Reserved keys cannot be used: {reserved}")
+
+        super().__init__(initial_data)
+        self._used_keys: set[K] = set()
+
+    def _check_key(self, key: K) -> None:
+        if key in self.RESERVED_KEYS:
+            raise KeyError(f"'{key}' is a reserved key and cannot be used.")
+
+    def __setitem__(self, key: K, value: V) -> None:
+        self._check_key(key)
+        super().__setitem__(key, value)
+
+    def __getitem__(self, key: K) -> V:
+        self._used_keys.add(key)
+        value = super().__getitem__(key)
+        return value
+
+    def update(self, *args, **kwargs) -> None:
+        """Override update to check for reserved keys"""
+        new_data = dict(*args, **kwargs)
+        if reserved := self.RESERVED_KEYS & new_data.keys():
+            raise KeyError(f"Reserved keys cannot be used: {reserved}")
+        super().update(new_data)
+
+
 @dataclass
 class Config:
     root: Path = ROOT
-    paths: dict[str, Path] = field(default_factory=dict)
+    paths: MemoryDict[str, Path] = field(default_factory=MemoryDict)
     clusters: dict[str, Cluster] = field(default_factory=dict)
     packages: list[str] = field(default_factory=list)
 
     @classmethod
     def from_dict(cls, data: Data):
-        config = DaciteConfig(type_hooks={Path: lambda v: Path(v).expanduser()})  # type: ignore
+        config = DaciteConfig(
+            type_hooks={Path: lambda v: Path(v).expanduser()}, cast=[MemoryDict]
+        )  # type: ignore
         return from_dict(cls, data, config)
 
     @classmethod
